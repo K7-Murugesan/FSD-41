f08b305f86ea93748a7071615fcbfa5214aa8ff1
Hemarajsgithub <hemaraj2708@gmail.com>
2024-05-20 18:28:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var fName = "xyz"
+
+// let fName = 10; //Numeric is not allowed as a first character
+var $fName = "abc"
+var _fName = "pqr"
+var FName = "123"
+var fname_ = "kesavan"
+
+console.log( fName, $fName, _fName, FName );
+console.log( fname_ );
+
+
+let newEmployeeId = 1; //camelCase
+
+let NewEmployeeId = 1; //Pascal Case
+
+let new_employee_id = 1; //under case or snake case
